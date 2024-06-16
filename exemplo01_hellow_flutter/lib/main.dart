d890aa4ca9715bfa8bf1c2749ad0b412420eecdc
Anderson Soares <andersonsoares@ufra.edu.br>
2024-06-15 22:03:31 -0300

    Criando uma classe homepage
    Criada uma classe para ser a HomePage que irá conter o scaffold.

--- exemplo01_hellow_flutter/lib/main.dart
+++ exemplo01_hellow_flutter/lib/main.dart
@@ -11,29 +11,34 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.blue),
       // O scaffold permite utilizar outros widgets do material design
-      home: home(),
+      home: HomePage(),
     );
   }
 }
 
-home() {
-  return Scaffold(
-    appBar: AppBar(
-      title: const Text('Barra de título !!'),
-      centerTitle: true,
-      backgroundColor: const Color.fromARGB(255, 88, 182, 230),
-    ),
-    body: Container(
-      //cor de fundo: azul aline blue
-      color: const Color.fromARGB(240, 248, 255, 255),
-    ),
-    //drawer: menu lateral esquerdo
-    drawer: Container(
-      color: const Color.fromARGB(255, 2, 38, 92),
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: () {},
-      child: const Icon(Icons.add),
-    ),
-  );
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Barra de título'),
+        centerTitle: true,
+        backgroundColor: const Color.fromARGB(255, 88, 182, 230),
+      ),
+      body: Container(
+        //cor de fundo: azul aline blue
+        color: const Color.fromARGB(240, 248, 255, 255),
+      ),
+      //drawer: menu lateral esquerdo
+      drawer: Container(
+        color: const Color.fromARGB(255, 2, 38, 92),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
 }
